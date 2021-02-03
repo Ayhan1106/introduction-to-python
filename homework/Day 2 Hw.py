76da61fb69a27ffd568b954a6fd1bd241468ca8b
Ayhan1106 <65809915+Ayhan1106@users.noreply.github.com>
2021-02-03 14:18:06 +0300

    Ayhan AYSOY

---
+++ homework/Day 2 Hw.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[75]:
+
+
+#create a list
+list = ["a","b","c","d","e","f"]
+#swap values
+i=0
+while i<3:
+    x = list[i]
+    list[i]= list[i+3]
+    list[i+3]= x
+    i +=1
+#print list    
+print(list)
+
+
+# In[76]:
+
+
+#create a value and promt the user
+a = (int(input("Give me a value: ")))
+#find even values
+for i in range (0,a,2):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
